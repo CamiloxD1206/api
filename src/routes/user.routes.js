cc3f642b4489bde73f974dc367a96ec2da11b122
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-10-21 00:49:05 -0500

    API

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,4 +12,42 @@ router.post("/register", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+//get all user
+router.get("/users", (req, res) => {
+    userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//get user
+router.get("/user/:id", (req, res) => {
+    const { id } = req.params;
+    userSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//update user
+router.put("/update/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, lastname, age, email } = req.body;
+    userSchema
+        .updateOne({ _id: id }, { $set: { name, lastname, age, email } })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//remove user
+router.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    userSchema
+        .deleteOne({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+
 module.exports = router;
